143088bdd4fe6456e41de9502e583759331f7b42
miranagha <m.agha46@gmail.com>
2018-06-19 22:22:46 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,25 +9,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Row, Col } from "reactstrap";
 
 const Home = () => (
-  <div>
-    <Head>
-      <title>London Removals - Top removal services get free quotes 24/7</title>
-      <meta name="description" content="Top London Removals is a specialist home, office and international removals company since 2001. You can request quotes for free 24/7 no obligations to receive the most competitive prices in the industry."/>
-      <meta name="google-site-verification" content="VsHncoHhcXRVYyv61NQAx_3Kz8YVDSY5jB20QTMZ6LE" />
-      <link rel="canonical" href="http://top-london-removals.com/" />
-    </Head>
-    <Page>
-      <Row>
-        <Col md="8">
-          <Section />
-        </Col>
-        <Col md="4">
-          <Form />
-          <AsideText />
-        </Col>
-      </Row>
-    </Page>
-  </div>
+
 );
 
 export default Home;
